7bf4d4b1a47d2bd071ddd09170a58c24c92f220b
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2020-04-28 12:14:52 -0400

    refactor

--- site.hs
+++ site.hs
@@ -5,62 +5,67 @@ cfg = defaultConfiguration
         { deployCommand = "./publish.sh"
         }
 
+static :: Rules ()
+static = do
+  match "images/*" $ do
+    route idRoute
+    compile copyFileCompiler
+  match "css/*" $ do
+    route idRoute
+    compile compressCssCompiler
+
+pages :: Rules ()
+pages = match (fromList ["about.md", "contact.md"]) $ do
+  route $ setExtension "html"
+  compile $ pandocCompiler
+    >>= loadAndApplyTemplate "templates/default.html" defaultContext
+    >>= relativizeUrls
+
+postPages :: Rules ()
+postPages = match "posts/*" $ do
+  route $ setExtension "html"
+  compile $ pandocCompiler
+    >>= loadAndApplyTemplate "templates/post.html" postCtx
+    >>= loadAndApplyTemplate "templates/default.html" postCtx
+    >>= relativizeUrls
+
+archive :: Rules ()
+archive = create ["archive.html"] $ do
+  route idRoute
+  compile $ do
+    posts <- recentFirst =<< loadAll "posts/*"
+    let archiveCtx = listField "posts" postCtx (return posts)
+                     `mappend` constField "title" "Archives"
+                     `mappend` defaultContext
+    makeItem ""
+      >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+      >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+      >>= relativizeUrls
+
+index :: Rules ()
+index = match "index.html" $ do
+  route idRoute
+  compile $ do
+    posts <- recentFirst =<< loadAll "posts/*"
+    let indexCtx = listField "posts" postCtx (return posts)
+                   `mappend` defaultContext
+    getResourceBody
+      >>= applyAsTemplate indexCtx
+      >>= loadAndApplyTemplate "templates/default.html" indexCtx
+      >>= relativizeUrls
+
+templates :: Rules ()
+templates = match "templates/*" $ compile templateBodyCompiler
+
+postCtx :: Context String
+postCtx = dateField "date" "%B %e, %Y"
+          `mappend` defaultContext
+
 main :: IO ()
 main = hakyllWith cfg $ do
-    match "images/*" $ do
-        route   idRoute
-        compile copyFileCompiler
-
-    match "css/*" $ do
-        route   idRoute
-        compile compressCssCompiler
-
-    match (fromList ["about.md", "contact.md"]) $ do
-        route   $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= relativizeUrls
-
-    match "posts/*" $ do
-        route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
-
-    create ["archive.html"] $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
-                    defaultContext
-
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
-
-
-    match "index.html" $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    defaultContext
-
-            getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
-                >>= relativizeUrls
-
-    match "templates/*" $ compile templateBodyCompiler
-
-
---------------------------------------------------------------------------------
-postCtx :: Context String
-postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
-    defaultContext
+  static
+  pages
+  postPages
+  archive
+  index
+  templates
